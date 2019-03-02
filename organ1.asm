4f057386f2c8e737b00582d11c50e469b9575a8c
Kazuhiro Ouchi <kanpapa@kanpapa.com>
2019-03-02 16:59:10 +0900

    Update comments

--- organ1.asm
+++ organ1.asm
@@ -6,33 +6,36 @@
 	.OR	$0000
 *
 START	BR	MAIN
-	.DB	$23
-	.DB	$26
-	.DB	$2B
-	.DB	$31
-	.DB	$38
-	.DB	$3B
-	.DB	$43
-	.DB	$4C
-
-MAIN	BN4	MAIN	IF EF4=0 MAIN 
-	LDI	#$2F	$2F->D
-	PLO	3	D->R(3).0
-	SEX	3	3->X
-	INP	4	BUS->M(R(3));N LINES=4
-	LDN	3	M(R(3))->D
-	ADI	2	D+2->D. DF
-	PLO	4	D->R(4).0
-	LDN	4	M(R(4))->D
-	PLO	5	D->R(5).0
-LOOP2	DEC	5	R(5)-1
-	GLO	5	R(5).0->D
-	BNZ	LOOP2	IF D!=0 LOOP2
-	BQ	LOOP3	IF Q=0 LOOP3
-	SEQ		1->Q
-	BR	MAIN
-
-LOOP3	REQ		0->Q
-	BR	MAIN
+*
+DATA	.DB	$23	;C
+	.DB	$26	;B
+	.DB	$2B	;A
+	.DB	$31	;G
+	.DB	$38	;F
+	.DB	$3B	;E
+	.DB	$43	;D
+	.DB	$4C	;C
+*
+MAIN	BN4	MAIN	;IF EF4=0 MAIN 
+	LDI	#IOR	;D <- #IOR
+	PLO	3	;R(3).0 <- D
+	SEX	3	;X <- 3
+	INP	4	;M(R(3)) <- BUS;N LINES=4
+	LDN	3	;D <- M(R(3))
+	ADI	DATA	;D,DF <- DATA+D
+	PLO	4	;R(4).0 <- D
+	LDN	4	;D <- M(R(4))
+	PLO	5	;R(5).0 <- D
+LOOP2	DEC	5	;R(5)--
+	GLO	5	;D <- R(5).0
+	BNZ	LOOP2	;IF D!=0 LOOP2
+	BQ	LOOP3	;IF Q=0 LOOP3
+	SEQ		;Q <- 1
+	BR	MAIN	;Branch to MAIN
+*
+LOOP3	REQ		;Q <- 0
+	BR	MAIN	;Branch to MAIN
+*
+IOR	.DB	0	;IO Register
 
 	.EN
