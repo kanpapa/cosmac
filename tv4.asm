b854057c57b0c8a55defb170ce7fe43f5705f215
Kazuhiro Ouchi <kanpapa@kanpapa.com>
2020-01-07 07:47:29 +0900

    add test program

---
+++ tv4.asm
@@ -0,0 +1,36 @@
+	.LF	tv4
+	.TF	tv4.hex,HEX
+*
+* TV Display program 4 for COSMAC
+* SB-Assembler
+*
+	.CR	1802	;To load the 1802 cross overlay	
+	.OR	$0000
+*
+START	LDI	4	;4 -> D
+	PLO	3	;D -> R(3).0
+	SEP	3	;3 -> P
+LOOP1	LDI	$40	;$40 -> D
+	PLO	4	;D -> R(4).0
+LOOP2	LDI	$20	;$20 -> D
+	PLO	0	;D -> R(0).0
+LOOP3	IDL
+	BN4	LOOP3
+	GLO	4
+	SHR
+	SHR
+	SHR
+	SHR
+	BDF	$19
+	LDI	$FF
+	STR	4
+	BR	$1C
+	LDI	$00
+	STR	4
+	INC	4
+	GLO	4
+	SMI	$F0
+	BNZ	$07
+	BR	$04
+
+	.EN
