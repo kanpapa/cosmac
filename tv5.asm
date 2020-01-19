e0f5f2df5e686daa8a2f0d085dc8e616b317a503
Kazuhiro Ouchi <kanpapa@kanpapa.com>
2020-01-19 13:09:27 +0900

    Update tv5.asm

--- tv5.asm
+++ tv5.asm
@@ -7,28 +7,28 @@
 	.CR	1802	;To load the 1802 cross overlay	
 	.OR	$0000
 *
-START	LDI	LOOP1	;LOOP1 -> D
-	PLO	3	;D -> R(3).0
-	LDI	0
+START	LDI	0	;initrize register R(3).1 R(4).1
 	PHI	3
-	SEP	3	;3 -> P
-LOOP1	LDI	$40	;$40 -> D
-	PLO	4	;D -> R(4).0
-	LDI	0
 	PHI	4
-LOOP2	LDI	$20	;$20 -> D
-	PLO	0	;D -> R(0).0
+	LDI	SETSTR	;SETSTR address R(3)
+	PLO	3
+	SEP	3	;Change Program Counter R(3)
+*
+SETSTR	LDI	$40	;Set data store address R(4)=$0040
+	PLO	4
+SETDMA	LDI	$20	;Set DMA address R(0)=$0020
+	PLO	0
 	LDI	0
 	PHI	0
-LOOP3	IDL
-	BN2	LOOP3
-	INC	5
+DWAIT	IDL		;DMA waiting
+	BN2	DWAIT	;Check VSYNC(EF2)
+	INC	5	;Counter R(5)++
 	GLO	5
-	STR	4
-	INC	4
-	GLO	4
+	STR	4	;Store count data
+	INC	4	;Store address R(4)++
+	GLO	4	;Check store address > $F0
 	SMI	$F0
-	BNZ	LOOP2
-	BR	LOOP1
+	BNZ	SETDMA
+	BR	SETSTR
 
 	.EN
